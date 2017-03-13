4395444cd5b3a0f129e1a0d2806bbb06f64be290
Moshe W <moshe.waisberg@gmail.com>
2017-03-13 12:53:57 +0200

    Twitter SDK.

---
+++ twitter/src/main/java/com/github/applicaster/twitter/TwitterSearch.java
@@ -0,0 +1,21 @@
+package com.github.applicaster.twitter;
+
+import android.content.Context;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Twitter search engine.
+ * Created by Moshe on 2017/03/13.
+ */
+public class TwitterSearch {
+
+    public static void init(Context context) {
+        TwitterAuthConfig authConfig = new TwitterAuthConfig("consumerKey", "consumerSecret");
+        Fabric.with(context, new TwitterCore(authConfig));
+    }
+
+}
